9155f09f1b2645099bf9b6072c8bf490b58e30c7
CodeBrat <CodeBrat@CodeBrat.localdomain>
2017-12-21 21:18:06 -0500

    FIRSTCOMMIT: first commit to github

---
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'app-recipe-detail',
+    templateUrl:'./recipe-detail.component.html',
+    styleUrls:['./recipe-detail.component.css']
+})
+
+export class RecipeDetailComponent {
+
+    constructor(){
+
+    }
+}
